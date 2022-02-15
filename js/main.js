4deabf6f928669e26c6961597105cd31e7ccc095
Luis Lopez <lueunbii98@gmail.com>
2022-02-15 01:56:57 -0500

    Completed the movement code

--- js/main.js
+++ js/main.js
@@ -43,19 +43,52 @@ function drawSnake(){
     snake.forEach(drawSnakePart);
 }
 
-// Moving the snake
+//function to change the direction of the snake
+function changeDirection(event){
+    const LEFT_KEY = 37; // setting the key codes
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const keyPressed = event.keyCode; // gets the key that trigger the event
+    
+    const goingUp = dy === 10; //verify in what direction the snake is
+    const goingDown = dy === -10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if (keyPressed === LEFT_KEY && !goingRight){ //check if the snake is in the opposite way
+        dx = -10;
+        dy = 0;
+    }
+    if (keyPressed === UP_KEY && !goingDown){
+        dx = 0;
+        dy = -10;
+    }
+    if(keyPressed === RIGHT_KEY && !goingLeft){
+        dx = 10;
+        dy = 0;
+    }
+    if(keyPressed == DOWN_KEY && !goingUp){
+        dx = 0;
+        dy = 10;
+    }
+}
+
+// function that sets the new coordinates
 function advanceSnake() {
     const head = {x: snake[0].x + dx, y: snake[0].y + dy};
 
-    snake.unshift(head);
+    snake.unshift(head);//add to the beggining of the array
 
-    snake.pop();
+    snake.pop();//delete the end of the array
 }
 
-//test
+//fisrt movement
 let dx = 10;
 let dy = 0;
 
+//function to draw the next movement
 function main() {
     setTimeout (function onTick(){ //set a delay when calling a function
         clearCanvas();
@@ -66,6 +99,9 @@ function main() {
         main();
     }, 100);
 }
+
+//works when a key is pressed
+document.addEventListener('keydown', changeDirection);
 
 drawSnake();
 main();
